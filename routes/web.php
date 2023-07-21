bd6e3052b5ec77aa4755f0c3f42ad580d93c04f9
shatudas <das.shatu2000@gmail.com>
2023-07-21 20:25:48 +0600

    middleware

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-  return view('auth/login');
-});
 
 
-Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-//-------user---------//
-Route::prefix('user')->group(function()
-{
-	Route::get('/view','Backend\UserController@view')->name('user.view');
-	Route::get('/add','Backend\UserController@add')->name('user.add');
-	Route::post('/store','Backend\UserController@store')->name('user.store');
-	Route::get('/edit/{id}','Backend\UserController@edit')->name('user.edit');
-	Route::post('/update/{id}','Backend\UserController@update')->name('user.update');
-	Route::get('/delete/{id}','Backend\UserController@delete')->name('user.delete');
-	Route::get('/active/{id}','Backend\UserController@active')->name('user.active');
-	Route::get('/inacative/{id}','Backend\UserController@inacative')->name('user.inacative');
+Auth::routes();
+	Route::get('/', function () {
+    return view('auth/login');
 });
+
+
+Route::group(['middleware' => 'auth'],function(){
+ //-------user---------//
+	Route::prefix('user')->group(function()
+	{
+		Route::get('/view','Backend\UserController@view')->name('user.view');
+		Route::get('/add','Backend\UserController@add')->name('user.add');
+		Route::post('/store','Backend\UserController@store')->name('user.store');
+		Route::get('/edit/{id}','Backend\UserController@edit')->name('user.edit');
+		Route::post('/update/{id}','Backend\UserController@update')->name('user.update');
+		Route::get('/delete/{id}','Backend\UserController@delete')->name('user.delete');
+		Route::get('/active/{id}','Backend\UserController@active')->name('user.active');
+		Route::get('/inacative/{id}','Backend\UserController@inacative')->name('user.inacative');
+	});
+
+
+	
+
+
+
+
+});
+
 
